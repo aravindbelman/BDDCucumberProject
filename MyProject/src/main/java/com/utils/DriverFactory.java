a43ef4fc5b82595988c73f2516ce6c9259cd49cc
aravindbelman <aravindbelman@gmail.com>
2025-09-02 18:32:53 +0530

    Updated driver factory

--- MyProject/src/main/java/com/utils/DriverFactory.java
+++ MyProject/src/main/java/com/utils/DriverFactory.java
@@ -4,18 +4,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class DriverFactory {
 
         private static WebDriver driver;
 
-        public static WebDriver getDriver() {
-            if (driver == null) {
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--start-maximized");
-                driver = new ChromeDriver(options);
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+
+            // Headless mode for CI/CD (GitHub Actions Linux)
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+
+            try {
+                // Create a unique temporary directory for user data
+                Path tempDir = Files.createTempDirectory("chrome-user-data");
+                options.addArguments("--user-data-dir=" + tempDir.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            return driver;
+
+            driver = new ChromeDriver(options);
         }
+        return driver;
+    }
 
         public static void quitDriver() {
             if (driver != null) {
